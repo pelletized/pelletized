dd7474f125ec91e65f9bd18e2d6aad8e67fc9257
Ed Wheeler <ewheeler3@gmail.com>
2016-08-15 09:20:28 -0700

    added photo page, minor animation on topnav

--- includes/header.php
+++ includes/header.php
@@ -2,47 +2,47 @@
 //keep ip addresses off my site
 $ip = $_SERVER['REMOTE_ADDR'];
 //print $ip;
-if ($ip == '90.196.84.255' || $ip == '71.13.75.131' || $ip == '70.176.128.138' || $ip == '80.0.6.49' || $ip == '37.229.224.119' || $ip == '190.229.241.111') { 
+if ($ip == '90.196.84.255' || $ip == '71.13.75.131' || $ip == '70.176.128.138' || $ip == '80.0.6.49' || $ip == '37.229.224.119' || $ip == '190.229.241.111') {
 	header("Location: busted.php");
 }
 ?>
 
 <?php include "head.php"; ?>
 <body id="<?=$page?>">
-<header class="global-header">	
+<header class="global-header">
 	<nav class="nav-main">
 		<div class="row">
 			<div class="col col-10">
-				<a href="/" class="logo"><img src="/images/pxd-logo-icon.png" width="36" height="40" alt="pelletized"></a> 
+				<a href="/" class="logo"><img src="/images/pxd-logo-icon.png" width="36" height="40" alt="pelletized"></a>
 			</div><!--/.col-->
-			<div class="col col-80 menu">				
+			<div class="col col-80 menu">
 				<ul>
 				<?php
 					if ($page == "home") {?>
 						<li><a href="/" class="active">Home</a></li>
 					<?php } else { ?>
 						<li><a href="/">Home</a></li>
-					<?php }	
+					<?php }
 					if ($page == "portfolio") {?>
 						<li><a href="/portfolio/" class="active">Portfolio</a></li>
 					<?php } else { ?>
 						<li><a href="/portfolio/">Portfolio</a></li>
-					<?php }	
+					<?php }
 					if ($page == "resume") {?>
 						<li><a href="/resume/" class="active">Resume</a></li>
 					<?php } else { ?>
 						<li><a href="/resume/">Resume</a></li>
-					<?php }	
-					if ($page == "testimonials") {?>
-						<!--<li><a href="/testimonials/">Testimonials</a></li>-->
+					<?php }
+					if ($page == "photos") {?>
+						<li><a href="/photos/">Photos</a></li>
 					<?php } else { ?>
-						<!--<li><a href="/testimonials/">Testimonials</a></li>-->
+						<li><a href="/photos/">Photos</a></li>
 					<?php }
 					if ($page == "contact") {?>
 						<li><a href="/contact/" class="active">Contact</a></li>
 					<?php } else { ?>
 						<li><a href="/contact/">Contact</a></li>
-					<?php }	?>	
+					<?php }	?>
 						<!--<li><a href="http://blog.pelletized.com">Blog</a></li>-->
 				</ul>
 				<p id="pageid"><?=$page?>
@@ -51,13 +51,13 @@ if ($ip == '90.196.84.255' || $ip == '71.13.75.131' || $ip == '70.176.128.138' |
 				<div id="hamburger">
 					<span></span>
 					<span></span>
-					<span></span>				
-				</div> 
+					<span></span>
+				</div>
 			</div><!--/.col-->
 		</div><!--/.row-->
-	</nav><!--/.nav-main-->	
-	
+	</nav><!--/.nav-main-->
+
 	<div class="row">
-		<h1><?=$h1;?></h1>		
-	</div>	
+		<h1><?=$h1;?></h1>
+	</div>
 </header><!--/.global-header-->
---
+++ photos/index.php
@@ -0,0 +1,43 @@
+<?php
+	$pagetitle = "Photos | Ed Wheeler - Web design & HTML/CSS development";
+	$page = "Photos";
+	$h1 = "Photos by Ed Wheeler on display";
+	include "../includes/header.php";
+?>
+
+<div class="main" role="main">
+	<div class="row-highlighted">
+		<div class="row">
+			<div class="col col-100">
+				<p>An occasional photographer, below are the shows and galleries I've been involved with. Contact me if you are interested in purchasing a print!</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<section class="col col-50">
+			<div class="port-item">
+				<h3>Somerville Toy Camera Festival</h3>
+				<span class="note">2016</span>
+				<img src="images/somerville.2016.jpg" alt="Russ & Daughters by Ed Wheeler" />
+				<em>Russ & Daughters</em><br>
+				Camera: Plastic Filmtastic Debonair				
+			</div>
+		</section>
+
+		<section class="col col-50">
+			<div class="port-item">
+				<h3>RayKo's 8th Annual International Juried Plastic Camera
+Show</h3>
+				<span class="note">2015</span>
+				<img src="images/rayko.2015.jpg" alt="Flatiron Building" />
+				<em>Flatiron Building</em><br>
+				Camera: Plastic Filmtastic Debonair
+			</div>
+		</section>
+
+	</div><!--/.row-->
+</div>
+<?php include "../includes/footer.php"; ?>
+</body>
+</html>
